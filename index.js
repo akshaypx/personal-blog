b8ef17a26d19223ad0cc27db993985183892a22f
akshaypx <akshaysrivastava46@gmail.com>
2024-09-12 22:41:58 +0530

    feat: basic authentication added

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import http from "http";
 import ejs from "ejs";
 import { getArticlesList, getArticle } from "./helper.js";
 import url from "url";
+import base64 from "base-64";
+
+function decodeCredentials(authHeader) {
+  //authHeader : Basic --------------
+  const encodedCredentials = authHeader.trim().replace(/Basic\s+/i, "");
+  const decodedCredentials = base64.decode(encodedCredentials);
+  return decodedCredentials.split(":");
+}
 
 var server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
@@ -23,6 +31,22 @@ var server = http.createServer((req, res) => {
       getArticle: getArticle(articleNumber),
     });
     res.end(htmlRenderized);
+  }
+
+  if (parsedUrl.pathname === "/admin") {
+    const [username, password] = decodeCredentials(
+      req.headers.authorization || ""
+    );
+
+    if (username === "admin" && password === "admin") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      let htmlData = fs.readFileSync("views/admin.ejs", "utf-8");
+      let htmlRenderized = ejs.render(htmlData, {});
+      res.end(htmlRenderized);
+    } else {
+      res.writeHead(401, { "WWW-Authenticate": "Basic realm='user_pages'" });
+      res.end("Authentication Required");
+    }
   }
 });
 
