73bbc72e57c2df622fbb78b534955d9afd70f87a
akshaypx <akshaysrivastava46@gmail.com>
2024-09-15 17:41:32 +0530

    feat: edit blogs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import ejs from "ejs";
 import { getArticlesList, getArticle } from "./helper.js";
 import url from "url";
 import base64 from "base-64";
+import queryString from "querystring";
 
 function decodeCredentials(authHeader) {
   //authHeader : Basic --------------
@@ -64,6 +65,58 @@ var server = http.createServer((req, res) => {
         getArticle: getArticle(articleNumber),
       });
       res.end(htmlRenderized);
+    } else {
+      res.writeHead(401, { "WWW-Authenticate": "Basic realm='user_pages'" });
+      res.end("Authentication Required");
+    }
+  }
+
+  if (parsedUrl.pathname.startsWith("/update/")) {
+    const [username, password] = decodeCredentials(
+      req.headers.authorization || ""
+    );
+
+    if (username === "admin" && password === "admin") {
+      // res.writeHead(200, { "Content-Type": "text/html" });
+      const queryStr = parsedUrl.query;
+      const data = {
+        id: parseInt(queryStr.id),
+        title: queryStr.title,
+        date: queryStr.date,
+        content: queryStr.content,
+      };
+      console.log(data);
+      //console.log(queryStr.title, queryStr.date, queryStr.content);
+      let articles = JSON.parse(fs.readFileSync("data/articles.json", "utf-8"));
+      let updatedArticles = articles.map((a, i) => {
+        if (a.id === data.id) {
+          return data;
+        } else return a;
+      });
+      let updated = 0;
+      fs.writeFile(
+        "data/articles.json",
+        JSON.stringify(updatedArticles),
+        (err) => {
+          if (err) {
+            console.log(err);
+            res.end("<h1>Error updating!</h1>");
+          } else {
+            console.log("Article updated successfully");
+            updated = 1;
+            res
+              .writeHead(301, {
+                location: `/admin`,
+              })
+              .end();
+          }
+        }
+      );
+      // if (updated) {
+      //   res.end("<h1>updated!</h1>");
+      // } else {
+      //   res.end("<h1>Error updating!</h1>");
+      // }
     } else {
       res.writeHead(401, { "WWW-Authenticate": "Basic realm='user_pages'" });
       res.end("Authentication Required");
